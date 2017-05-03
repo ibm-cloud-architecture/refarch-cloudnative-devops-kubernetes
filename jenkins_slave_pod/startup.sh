a632110bddf318aa30114c0a718174641b621fe9
Fabio Gomez <fabiogomez@us.ibm.com>
2017-05-02 20:55:14 -0500

    Initial work on bluecompute chart and helm charts repo. Along with some changes to slave pod configuration

--- jenkins_slave_pod/startup.sh
+++ jenkins_slave_pod/startup.sh
@@ -2,13 +2,10 @@
 
 # Getting config values from secret
 export BLUEMIX_API_KEY=$(cat /var/run/secrets/bx-auth-secret/BLUEMIX_API_KEY)
-CF_ACCOUNT=$(cat /var/run/secrets/bx-auth-secret/CF_ACCOUNT)
-CF_ORG=$(cat /var/run/secrets/bx-auth-secret/CF_ORG)
-CF_SPACE=$(cat /var/run/secrets/bx-auth-secret/CF_SPACE)
 
 # Login to Bluemix CLI
 printf "\n\nLogging into Bluemix CLI\n"
-bx login -a api.ng.bluemix.net -c ${CF_ACCOUNT} -o ${CF_ORG} -s ${CF_SPACE}
+bx login
 
 # Init Container Service
 printf "\n\nInitializing plug-ins\n"
@@ -18,5 +15,6 @@ bx cr login
 export KUBE_API_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 export REGISTRY_NAMESPACE=$(bx cr namespace-list | egrep -v 'Listing namespaces...' | egrep -v '^OK$' | sed -e '/^Namespace   $/d' | sed -e '/^\s*$/d' | tr -d '[:space:]')
 
+helm init
 # This keeps the container alive
 exec "$@"
