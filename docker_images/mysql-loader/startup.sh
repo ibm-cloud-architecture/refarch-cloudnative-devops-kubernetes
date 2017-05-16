6948512ead287bbd8210c646711a4981fc448ea2
Fabio Gomez <fabiogomez@us.ibm.com>
2017-05-16 14:56:04 -0500

    Added mysql loader image

---
+++ docker_images/mysql-loader/startup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# URI parsing function
+#
+# The function creates global variables with the parsed results.
+# It returns 0 if parsing was successful or non-zero otherwise.
+#
+# [schema://][user[:password]@]host[:port][/path][?[arg1=val1]...][#fragment]
+#
+# from http://vpalos.com/537/uri-parsing-using-bash-built-in-features/
+#
+function uri_parser() {
+    # uri capture
+    uri="$@"
+
+    # safe escaping
+    uri="${uri//\`/%60}"
+    uri="${uri//\"/%22}"
+
+    # top level parsing
+    pattern='^(([a-z]{3,5})://)?((([^:\/]+)(:([^@\/]*))?@)?([^:\/?]+)(:([0-9]+))?)(\/[^?]*)?(\?[^#]*)?(#.*)?$'
+    [[ "$uri" =~ $pattern ]] || return 1;
+
+    # component extraction
+    uri=${BASH_REMATCH[0]}
+    uri_schema=${BASH_REMATCH[2]}
+    uri_address=${BASH_REMATCH[3]}
+    uri_user=${BASH_REMATCH[5]}
+    uri_password=${BASH_REMATCH[7]}
+    uri_host=${BASH_REMATCH[8]}
+    uri_port=${BASH_REMATCH[10]}
+    uri_path=${BASH_REMATCH[11]}
+    uri_query=${BASH_REMATCH[12]}
+    uri_fragment=${BASH_REMATCH[13]}
+
+    # path parsing
+    count=0
+    path="$uri_path"
+    pattern='^/+([^/]+)'
+    while [[ $path =~ $pattern ]]; do
+        eval "uri_parts[$count]=\"${BASH_REMATCH[1]}\""
+        path="${path:${#BASH_REMATCH[0]}}"
+        let count++
+    done
+
+    # query parsing
+    count=0
+    query="$uri_query"
+    pattern='^[?&]+([^= ]+)(=([^&]*))?'
+    while [[ $query =~ $pattern ]]; do
+        eval "uri_args[$count]=\"${BASH_REMATCH[1]}\""
+        eval "uri_arg_${BASH_REMATCH[1]}=\"${BASH_REMATCH[3]}\""
+        query="${query:${#BASH_REMATCH[0]}}"
+        let count++
+    done
+
+    # return success
+    return 0
+}
+
+
+# Checks for mysql "variable" set by Kubernetes secret
+if [ -z ${mysql+x} ]; then 
+    echo "Secret not in \"mysql\" variable. Aborting...";
+    exit 1
+else 
+    echo "Found mysql secret"
+    mysql_uri=$(echo $mysql | jq .uri | sed s%\"%%g)
+
+    # Do the URL parsing
+    uri_parser $mysql_uri
+
+    # Construct elasticsearch url
+    mysql_url="jdbc:${uri_schema}://${uri_host}:${uri_port}/inventorydb"
+    mysql_user=${uri_user}
+    mysql_password=${uri_password}
+    mysql_host=${uri_host}
+    mysql_port=${uri_port}
+
+    echo "Loading MySQL database..."
+    ./load-data.sh ${mysql_user} ${mysql_password} ${mysql_host} ${mysql_port} inventorydb
+fi
