11512896bb0e4a930fedc73915e22eaa7a312219
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-05-25 15:31:51 -0400

    smaller image, break out helper function into separate shell script

---
+++ docker_images/mysql-loader/helper.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# URI parsing function
+#
+# The function creates global variables with the parsed results.
+# It returns 0 if parsing was successful or non-zero otherwise.
+#
+# [schema://][user[:password]@]host[:port][/path][?[arg1=val1]...][#fragment]
+#
+# from http://vpalos.com/537/uri-parsing-using-bash-built-in-features/
+#
+function uri_parser() {
+    # uri capture
+    uri="$@"
+
+    # safe escaping
+    uri="${uri//\`/%60}"
+    uri="${uri//\"/%22}"
+
+    # top level parsing
+    pattern='^(([a-z]{3,5})://)?((([^:\/]+)(:([^@\/]*))?@)?([^:\/?]+)(:([0-9]+))?)(\/[^?]*)?(\?[^#]*)?(#.*)?$'
+    [[ "$uri" =~ $pattern ]] || return 1;
+
+    # component extraction
+    export uri=${BASH_REMATCH[0]}
+    export uri_schema=${BASH_REMATCH[2]}
+    export uri_address=${BASH_REMATCH[3]}
+    export uri_user=${BASH_REMATCH[5]}
+    export uri_password=${BASH_REMATCH[7]}
+    export uri_host=${BASH_REMATCH[8]}
+    export uri_port=${BASH_REMATCH[10]}
+    export uri_path=${BASH_REMATCH[11]}
+    export uri_query=${BASH_REMATCH[12]}
+    export uri_fragment=${BASH_REMATCH[13]}
+
+    # path parsing
+    count=0
+    path="$uri_path"
+    pattern='^/+([^/]+)'
+    while [[ $path =~ $pattern ]]; do
+        eval "uri_parts[$count]=\"${BASH_REMATCH[1]}\""
+        path="${path:${#BASH_REMATCH[0]}}"
+        let count++
+    done
+
+    # query parsing
+    count=0
+    query="$uri_query"
+    pattern='^[?&]+([^= ]+)(=([^&]*))?'
+    while [[ $query =~ $pattern ]]; do
+        eval "uri_args[$count]=\"${BASH_REMATCH[1]}\""
+        eval "uri_arg_${BASH_REMATCH[1]}=\"${BASH_REMATCH[3]}\""
+        query="${query:${#BASH_REMATCH[0]}}"
+        let count++
+    done
+
+    # return success
+    return 0
+}
