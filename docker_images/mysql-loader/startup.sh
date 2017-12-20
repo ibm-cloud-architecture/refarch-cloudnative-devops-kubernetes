fe9c3b4a624960567ba7f49e6dfb019f8c4e5437
Fabio Gomez <fabiogomez@us.ibm.com>
2017-12-20 14:16:28 -0500

    - Include load-data.sql in the Docker image - Use the included load-data.sql instead of downloading from web to avoid ICP issues when no access to internet

--- docker_images/mysql-loader/startup.sh
+++ docker_images/mysql-loader/startup.sh
@@ -64,21 +64,21 @@ if [ -z "${SQL_URL}" ]; then
     exit 0
 fi
 
-wget ${SQL_URL} -O load-data.sql
-if [ $? -ne 0 ]; then
-    echo "Failed to download ${SQL_URL}"
-    exit 1
-fi
+#wget ${SQL_URL} -O /load-data.sql
+#if [ $? -ne 0 ]; then
+#    echo "Failed to download ${SQL_URL}"
+#    exit 1
+#fi
 
 echo "Executing MySQL script ${SQL_URL} on MySQL database ${mysql_host}:${mysql_port} ..."
 
 # load data
-while !(mysql -v -u${mysql_user} -p${mysql_password} --host ${mysql_host} --port ${mysql_port} <load-data.sql)
+while !(mysql -v -u${mysql_user} -p${mysql_password} --host ${mysql_host} --port ${mysql_port} </load-data.sql)
 do
   printf "Waiting for MySQL to fully initialize\n\n"
   sleep 1
     echo "trying to load data again"
 done
 
-#rm load-data.sql testdata
+#rm /load-data.sql testdata
 printf "\n\nExecuted script at ${SQL_URL} on database %s\n\n" "${mysql_database}"
